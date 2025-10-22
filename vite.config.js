46860475f1da0979367f595f814ded24195243f3
Tolantt <61566945+Tolantt@users.noreply.github.com>
2025-10-22 15:19:00 +0800

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/financial-ai-app/', // 这里一定要和你的仓库名一致，前后都有斜杠
+  base: '/financial-ai-app/',
 })
